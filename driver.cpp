7781bf4de54409236fce8b79679346c206605ace
Benjamin Clark <benrc257@gmail.com>
2024-11-08 23:09:45 -0600

    small edits

--- driver.cpp
+++ driver.cpp
@@ -22,11 +22,11 @@ int main() {
                 pokedex.display();
                 break;
             case 2 : // Add a Pokemon
-            cout << "\n enter the name of the pokemon\n";
-            getline(cin,name);
-            cout << "\n enter the id for the pokemon\n";
-            getline(cin,id);
-            pokedex.appendPokemon(name,stoi(id));
+                cout << "\n enter the name of the pokemon\n";
+                getline(cin,name);
+                cout << "\n enter the id for the pokemon\n";
+                getline(cin,id);
+                pokedex.appendPokemon(name,stoi(id));
             break;
             case 3 : // Remove a Pokemon
                 cout << "\n enter the id of the pokemon you wish to remove\n";
@@ -36,9 +36,7 @@ int main() {
             case 4 : // Add Pokemon From File
                 cout << "\n enter the name of the file you want to import from\n";
                 getline(cin,name);
-                cout << "\n enter the delimiter used to separate values\n";
-                getline(cin,id);
-                pokedex.import(name,id.at(0));
+                pokedex.import(name,'#');
                 break;   
             case 5 : // END
                 cout << LINE << "Shutting down";
